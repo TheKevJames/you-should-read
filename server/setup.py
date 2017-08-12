94bf43193f7c7cc4b85ceeb1f14b2be6df654f71
Kevin James <kevin@talkiq.com>
2017-08-12 02:54:58 -0700

    docs(readthedocs): create package structure for rtd

---
+++ server/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import setuptools
+
+
+setuptools.setup(
+    name='you-should-read-server',
+    author='Kevin James',
+    author_email='KevinJames@thekev.in',
+    url='https://github.com/TheKevJames/you-should-read.git',
+    packages=setuptools.find_packages(),
+    install_requires=['sanic'],
+)
