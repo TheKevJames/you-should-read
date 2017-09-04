de450987253726c8205832e716517673863d451e
Kevin James <kevin@talkiq.com>
2017-09-04 00:42:04 -0700

    chore(deploy): deploy to docker stack

--- server/server/config.py
+++ server/server/config.py
@@ -1,5 +1,8 @@
 import os
 
 
-DATABASE_URL = os.environ.get('DATABASE_URL',
-                              'postgres://postgres@database:5432/postgres')
+try:
+    DATABASE_URL = open('/run/secrets/database_url').read().rstrip()
+except FileNotFoundError:
+    DATABASE_URL = os.environ.get('DATABASE_URL',
+                                  'postgres://postgres@database:5432/postgres')
