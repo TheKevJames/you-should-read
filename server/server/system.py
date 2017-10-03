d3c7df9f5dbe31cfbab6ed2cfcfb3cf62ac03009
Kevin James <kevin@talkiq.com>
2017-10-03 02:05:33 -0700

    feat(ping): allow HEAD calls to /ping
    fixes YOU-SHOULD-READ-SERVER-4

--- server/server/system.py
+++ server/server/system.py
@@ -4,7 +4,7 @@ import sanic
 system = sanic.Blueprint('system')
 
 
-@system.route('/ping')
+@system.route('/ping', methods=['GET', 'HEAD'])
 async def ping(_request):
     """Simple healthcheck."""
     return sanic.response.text('pong')
