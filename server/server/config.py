78b68e53ef8c0d5cbf9e4c2af83e2ea6d88e0320
Kevin James <kevin@talkiq.com>
2017-10-03 00:19:04 -0700

    fix(monitoring): fix sentry config typo

--- server/server/config.py
+++ server/server/config.py
@@ -10,7 +10,7 @@ except FileNotFoundError:
                                   'postgres://postgres@database:5432/postgres')
 
 try:
-    SENTRY_DSN = open('/run/secrets/secntry_dsn_ysr_server').read().rstrip()
+    SENTRY_DSN = open('/run/secrets/sentry_dsn_ysr_server').read().rstrip()
 except FileNotFoundError:
     SENTRY_DSN = None
 
