8276be10c5e58f0dfcbe7642e6c622ca5ce07f24
Kevin James <kevin@talkiq.com>
2017-10-02 23:24:20 -0700

    feat(sentry): install sentry
    fixes #54

--- server/server/config.py
+++ server/server/config.py
@@ -1,8 +1,17 @@
 import os
 
+import raven
+
 
 try:
     DATABASE_URL = open('/run/secrets/database_url').read().rstrip()
 except FileNotFoundError:
     DATABASE_URL = os.environ.get('DATABASE_URL',
                                   'postgres://postgres@database:5432/postgres')
+
+try:
+    SENTRY_DSN = open('/run/secrets/secntry_dsn_ysr_server').read().rstrip()
+except FileNotFoundError:
+    SENTRY_DSN = None
+
+sentry = raven.Client(SENTRY_DSN)
